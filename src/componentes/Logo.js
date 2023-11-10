0016e460956e6ebbbe4877910c57e0a6c3979a40
Miguel <vigoya2001@hotmail.com>
2023-11-10 15:37:49 -0500

    subiendo proyecto

---
+++ src/componentes/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Logo() {
+  return (
+    <div className='Logo'>
+    <h1>MIGUEL ANGEL VIGOYA BOLIVAR.</h1>
+    </div>
+  )
+}
+
+export default Logo
